a1e04051972c0cfe0cffe4293e6481380e4ca1b4
kesera2 <developer@kesera2.dev>
2025-10-02 19:51:48 +0900

    Fix: Remove UIToolkit CreateInspectorGUI to use IMGUI version
    UIToolkit's ListView doesn't show add/remove buttons correctly.
    This fix removes the CreateInspectorGUI() method to fallback to
    the IMGUI OnInspectorGUI() which properly displays the buttons.

--- Code/Editor/Mesh/DeformableEditorExtention.cs
+++ Code/Editor/Mesh/DeformableEditorExtention.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,39 +16,6 @@ namespace DeformEditor
 		{
 			deformerList?.Dispose();
 			deformerList = null;
-		}
-		
-		public override VisualElement CreateInspectorGUI() {
-			var root = base.CreateInspectorGUI() ?? new VisualElement();
-
-			root.Add (new IMGUIContainer(DrawIMGUI(DrawMainSettings)){style = {
-				marginBottom = 10
-			}});
-			
-			//root.Add (new IMGUIContainer(DrawIMGUI(DrawDeformersList)){style = {
-			//	marginBottom = 10
-			//}});
-			new ReorderableComponentElementList<Deform.Deformer>(root, serializedObject, serializedObject.FindProperty("deformerElements"));
-			root.Q<ListView>().Q<Toggle>().pickingMode = PickingMode.Ignore;
-			root.Q<ListView>().Q<Toggle>().Query<VisualElement>().ForEach(ve => ve.pickingMode = PickingMode.Ignore);
-
-			root.Add (new IMGUIContainer(DrawIMGUI(DrawUtilityToolbar)){style = {
-				marginBottom = 10
-			}});
-			
-			root.Add (new IMGUIContainer(DrawIMGUI(DrawDebugInfo)));
-			root.Add (new IMGUIContainer(DrawIMGUI(DrawHelpBoxes)));
-			
-			return root;
-		}
-		
-		Action DrawIMGUI(Action onGUIHandler){
-			return () => {
-			serializedObject.UpdateIfRequiredOrScript();
-			onGUIHandler();
-			if (serializedObject.ApplyModifiedProperties())
-				EditorApplication.QueuePlayerLoopUpdate();
-			};
 		}
 	}
 }
